ecf0f242bc085145a973faada9849c6db0bac24a
akbaar32 <muhammad.akbaar32@gmail.com>
2018-01-17 17:37:17 +0800

    membuat output

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -26,6 +26,19 @@ public:
         SpeciesBeruang::beratbadan 			= beratbadan;
         SpeciesBeruang::tinggibadan 		= tinggibadan;
         SpeciesBeruang::umurberuang 		= umurberuang;
-    }
-
+    }   
+    void Output (void)
+    {
+    	cout << "###############################################################################" << endl;
+    	cout << "*********************************SPECIES BERUANG*******************************"<< endl;
+        cout << "Nama Beruang           	: " << namaberuang << endl;
+        cout << "Nama Ilmiah           	: " << namailmiah << endl;
+        cout << "Habitat Beruang        	: " << habitatberuang << endl;
+        cout << "Warna Bulu Beruang     	: " << warnabuluberuang << endl;
+        cout << "Jenis Beruang      	: " << jenisberuang << endl;
+        cout << "Berat Badan      	: " << beratbadan << " Kg" << endl;
+        cout << "Tinggi Badan  		: " << tinggibadan << " Meter" <<endl;
+        cout << "Umur Beruang     	: " << umurberuang << "Tahun" << endl;
+        cout << endl;
+    };
 };
