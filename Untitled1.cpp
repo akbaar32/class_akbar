a5a59433555382eb845246b25a69b612bbdc3b65
akbaar32 <muhammad.akbaar32@gmail.com>
2018-01-17 17:34:11 +0800

    pemberian variable terhadapa class baru yaitu species beruang

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class SpeciesBeruang
+{	
+private:
+	string namaberuang;
+	string namailmiah;
+    string habitatberuang;
+    string warnabuluberuang;
+    string jenisberuang;
+    int beratbadan;
+    int tinggibadan;
+    int umurberuang;
+};
