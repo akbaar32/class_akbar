6aa85279813f098a0dbf7f4a19de8fa740fd1888
akbaar32 <muhammad.akbaar32@gmail.com>
2018-01-17 17:36:10 +0800

    pemberian isi dari variable yang di buat atau di deklarasikan pada variable di atasnya (pemberian nilai value)

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -13,4 +13,19 @@ private:
     int beratbadan;
     int tinggibadan;
     int umurberuang;
+    
+public:
+    SpeciesBeruang (string namaberuang, string namailmiah, string habitatberuang, string warnabuluberuang, string jenisberuang, 
+	int beratbadan, int tinggibadan, int umurberuang)
+    {
+    	SpeciesBeruang::namaberuang 		= namaberuang;
+    	SpeciesBeruang::namailmiah 			= namailmiah;
+        SpeciesBeruang::habitatberuang 		= habitatberuang;
+        SpeciesBeruang::warnabuluberuang 	= warnabuluberuang;
+        SpeciesBeruang::jenisberuang 		= jenisberuang;
+        SpeciesBeruang::beratbadan 			= beratbadan;
+        SpeciesBeruang::tinggibadan 		= tinggibadan;
+        SpeciesBeruang::umurberuang 		= umurberuang;
+    }
+
 };
